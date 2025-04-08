53c17315638e897835fdca40a177d974d43bd33f
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2025-04-08 11:18:33 +0530

    CSV DATA HANDLING

---
+++ src/main/java/com/bridgelabz/basic/ReadAndPrint.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.basic;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadAndPrint {
+    public static void main(String[] args) {
+        try(BufferedReader reader=new BufferedReader(new FileReader("C:\\Users\\Manvi\\Desktop\\java-csv-data-handling\\src\\main\\java\\com\\bridgelabz\\basic\\students.csv"))){
+            reader.readLine();
+            String line;
+            while((line=reader.readLine())!=null){
+                String columns[]=line.split(",");
+                System.out.println("ID: "+columns[0]+", Name: "+columns[1]+", Age: "+columns[2]+", Marks: "+columns[3]);
+
+            }
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+}
